9940e32f3358000bd3297288dc78398482d4214f
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2024-10-27 22:35:09 +0400

    Добавление кода

---
+++ lab_2/pythonProject2/return_path.py
@@ -0,0 +1,17 @@
+from iterator import Iterator
+
+
+def get_path(file_name, class_name):
+
+    i = Iterator(file_name, class_name)
+    for val in i:
+        if (val != None):
+            print(val)
+
+    print('Готово')
+
+
+if __name__ == "__main__":
+    file_name = 'dataset.csv'
+    class_name = 'good'
+    get_path(file_name, class_name)
